ec2e673e08b7db0f629245cedf7bdae70a001242
theuspimenta <thedohenrique@gmail.com>
2024-01-03 17:47:19 -0300

    Adicionado Lógica para habilitar ou desabilitar caracters especiais

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import random
 import string
 
-def gerar_senha(comprimento):
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha = ''.join(random.choice(caracteres) for i in range(comprimento))
+def gerar_senha(comprimento, incluir_especiais=True):
+    caracteres = string.ascii_letters + string.digits
+    if incluir_especiais:
+        caracteres += string.punctuation
+
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
     return senha
 
 # Exemplo de uso
 comprimento_senha = 12
-senha_gerada = gerar_senha(comprimento_senha)
+incluir_especiais = False  # Altere para False se não quiser caracteres especiais
+senha_gerada = gerar_senha(comprimento_senha, incluir_especiais)
 print("Senha Gerada:", senha_gerada)
